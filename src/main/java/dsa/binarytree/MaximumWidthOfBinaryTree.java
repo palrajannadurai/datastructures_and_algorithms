4c5ef46ccf7e3c918b246e96674095c99f68b257
palraj <palrajannadurai@gmail.com>
2026-01-09 09:33:05 +0530

    binary tree

--- src/main/java/dsa/binarytree/MaximumWidthOfBinaryTree.java
+++ src/main/java/dsa/binarytree/MaximumWidthOfBinaryTree.java
@@ -8,69 +8,50 @@ import java.util.Queue;
 public class MaximumWidthOfBinaryTree {
 
     static class Pair {
-        int index;
+        long index;
         TreeNode node;
 
-        public Pair(int index, TreeNode node) {
+        public Pair(long index, TreeNode node) {
             this.index = index;
             this.node = node;
         }
-    }
 
-    public int widthOfBinaryTreeOptimized(TreeNode root) {
-        Queue<Pair> q = new LinkedList<>();
-        int ans = 0;
-        q.add(new Pair(0, root));
-        while (!q.isEmpty()) {
-            int size = q.size();
-            int min = q.peek().index;
-            int first = 0, last = 0;
-            for (int i = 1; i <= size; i++) {
-                Pair it = q.remove();
-                TreeNode node = it.node;
-                int curr_idx = it.index - min;
-                if (i == 1)
-                    first = curr_idx;
-                if (i == size)
-                    last = curr_idx;
-
-                if (node.left != null) {
-                    q.add(new Pair(curr_idx * 2 + 1, node.left));
-                }
-                if (node.right != null) {
-                    q.add(new Pair(curr_idx * 2 + 2, node.right));
-                }
-            }
-            ans = Math.max(ans, last-first+1);
+        @Override
+        public String toString() {
+            return "Pair{" +
+                    "index=" + index +
+                    ", node=" + node +
+                    '}';
         }
-        return ans;
     }
 
     public int widthOfBinaryTree(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        int maxWidth = 0;
         Deque<Pair> bfsQueue = new LinkedList<>();
         bfsQueue.offer(new Pair(0, root));
+        long ans = 0;
         while (!bfsQueue.isEmpty()) {
+            System.out.println(bfsQueue);
             int size = bfsQueue.size();
-            int startIndex = bfsQueue.peek().index;
-            int lastIndex = startIndex;
+            long base = bfsQueue.peek().index;
+            long start = 0;
+            long last = 0;
             for (int i = 0; i < size; i++) {
                 Pair current = bfsQueue.poll();
-                int index = current.index - startIndex;
-                lastIndex = index;
-                if (current.node.left != null) {
-                    bfsQueue.offer(new Pair(2 * index, current.node.left));
-                }
+                if (i == 0) start = current.index - base;
+                if (i == size - 1) last = base - current.index;
                 if (current.node.right != null) {
-                    bfsQueue.offer(new Pair(2 * index + 1, current.node.right));
+                    bfsQueue.offer(new Pair(2l * current.index + 1L, current.node.right));
+                }
+                if (current.node.left != null) {
+                    bfsQueue.offer(new Pair(2 * current.index, current.node.left));
                 }
             }
-            maxWidth = Math.max(maxWidth, lastIndex + 1);
+            ans = Math.max(ans, (last - start + 1));
         }
-        return maxWidth;
+        return Math.toIntExact(ans);
     }
 
     public static void main(String[] args) {
