cb3c0d9c80e3ca4bf768b071fd8a070443869eb2
palraj <palrajannadurai@gmail.com>
2026-01-22 00:32:52 +0530

    dp & revision

--- src/main/java/dsa/array/SetMatrixZeroes.java
+++ src/main/java/dsa/array/SetMatrixZeroes.java
@@ -3,64 +3,29 @@ package dsa.array;
 import java.util.Arrays;
 
 public class SetMatrixZeroes {
-    // Method to set zeroes in the matrix based on the positions of zeros
-//    public static void setZeroes(int[][] matrix) {
-//        // Check if the matrix is empty or has no rows/columns
-//        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-//            return; // If so, return without modifying the matrix
-//        }
-//
-//        int rowLength = matrix.length;
-//        int columnLength = matrix[0].length;
-//
-//        // Arrays to track which rows and columns contain zeros
-//        int[] row = new int[rowLength];
-//        int[] column = new int[columnLength];
-//
-//        // Loop through the matrix to identify zeros and mark their rows and columns
-//        for (int i = 0; i < rowLength; i++) {
-//            for (int j = 0; j < columnLength; j++) {
-//                if (matrix[i][j] == 0) {
-//                    row[i] = 1; // Mark the row
-//                    column[j] = 1; // Mark the column
-//                }
-//            }
-//        }
-//
-//        // Loop through the matrix again to set zeros in rows and columns based on the tracking arrays
-//        for (int i = 0; i < rowLength; i++) {
-//            for (int j = 0; j < columnLength; j++) {
-//                if (row[i] == 1 || column[j] == 1) {
-//                    matrix[i][j] = 0; // Set the element to zero if its row or column contains a zero
-//                }
-//            }
-//        }
-//    }
 
     public static void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
 
+        boolean hasFirstColumnsContainsZero = false;
         boolean hasFirstRowContainsZero = false;
-        boolean hasFirstColumnContainsZero = false;
 
-        // 1. check first row has zero
-        for (int i = 0; i < n; i++) {
-            if (matrix[0][i] == 0) {
+        for (int row : matrix[0]) {
+            if (row == 0) {
                 hasFirstRowContainsZero = true;
                 break;
             }
         }
 
-        // 2. check first column has zero
         for (int[] ints : matrix) {
             if (ints[0] == 0) {
-                hasFirstColumnContainsZero = true;
+                hasFirstColumnsContainsZero = true;
                 break;
             }
         }
-
-        // 3. Use first row and column as a marker
+//        m = 3 - 1= 2
+//        n = 4 - 1 = 3
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
@@ -78,27 +43,38 @@ public class SetMatrixZeroes {
             }
         }
 
-        if (hasFirstRowContainsZero) {
-            for (int i = 0; i < n; i++) {
-                matrix[0][i] = 0;
-            }
-        }
-
-        if (hasFirstColumnContainsZero) {
+        if (hasFirstColumnsContainsZero) {
             for (int i = 0; i < m; i++) {
                 matrix[i][0] = 0;
             }
         }
 
-        System.out.println("hasFirstRowContainsZero : " + hasFirstRowContainsZero);
-        System.out.println("hasFirstColumnContainsZero : " + hasFirstColumnContainsZero);
+        if (hasFirstRowContainsZero) {
+            for (int i = 0; i < n; i++) {
+                matrix[0][i] = 0;
+            }
+        }
     }
 
     public static void main(String[] args) {
-//        int[][] matrix = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
-        int[][] matrix = {{1,1,1},{1,0,1},{1,1,1}};
+        int[][] matrix = {
+                {1, 1, 2, 8},
+                {3, 4, 5, 2},
+                {1, 3, 1, 0}
+        };
+/*        int[][] matrix = {
+                {1, 1, 1},
+                {1, 0, 1},
+                {1, 1, 1}
+        };*/
+        for (int[] nums: matrix) {
+            System.out.println(Arrays.toString(nums));
+        }
+        System.out.println("*********************");
         setZeroes(matrix);
-        System.out.println(Arrays.deepToString(matrix));
+        for (int[] nums: matrix) {
+            System.out.println(Arrays.toString(nums));
+        }
     }
 }
 
