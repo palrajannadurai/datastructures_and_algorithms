cabc9e97f55aa2616379f3ae9d0ef3aa4aa08e0b
palraj <palrajannadurai@gmail.com>
2026-02-05 19:04:40 +0530

    word search

--- src/main/java/dsa/array/WordSearch.java
+++ src/main/java/dsa/array/WordSearch.java
@@ -2,17 +2,19 @@ package dsa.array;
 
 public class WordSearch {
 
+    private static final int ASCII_SIZE = 128;
+    private static final int XOR_MASK = 256;
     private static final int[][] DIRECTIONS = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
 
     public boolean exist(char[][] board, String word) {
-        final int m = board.length;
-        final int n = board[0].length;
+        final int row = board.length;
+        final int col = board[0].length;
 
-        if (m * n < word.length()) return false;
+        if (row * col < word.length()) return false;
 
-        int[] freq = new int[128];
-        for (char[] row : board) {
-            for (char c : row) {
+        int[] freq = new int[ASCII_SIZE];
+        for (char[] r : board) {
+            for (char c : r) {
                 freq[c]++;
             }
         }
@@ -22,8 +24,12 @@ public class WordSearch {
             }
         }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        if (freq[word.charAt(0)] > freq[word.charAt(word.length() - 1)]) {
+            word = new StringBuilder(word).reverse().toString();
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 if (board[i][j] == word.charAt(0) && dfs(board, word, i, j, 0)) {
                     return true;
                 }
@@ -32,28 +38,30 @@ public class WordSearch {
         return false;
     }
 
-    private boolean dfs(char[][] board, String word, int i, int j, int index) {
+    private boolean dfs(char[][] board, String word, int row, int col, int index) {
         if (index == word.length()) return true;
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) {
             return false;
         }
-        if (word.charAt(index) != board[i][j]) {
+        if (word.charAt(index) != board[row][col]) {
             return false;
         }
-        board[i][j] ^= 256;
-        boolean found = false;
+        board[row][col] ^= XOR_MASK;
+
         for(int[] direction: DIRECTIONS) {
-            found = dfs(board, word, i + direction[0], j + direction[1], index + 1);
-            if (found) break;
+            if (dfs(board, word, row + direction[0], col + direction[1], index + 1)) {
+                board[row][col] ^= XOR_MASK;
+                return true;
+            }
         }
-        board[i][j] ^= 256;
-        return found;
+        board[row][col] ^= XOR_MASK;
+        return false;
     }
 
 
     public static void main(String[] args) {
         char[][] board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
         String word = "ABCCED";
-        System.out.println();
+        System.out.println( new WordSearch().exist(board, word));
     }
 }
