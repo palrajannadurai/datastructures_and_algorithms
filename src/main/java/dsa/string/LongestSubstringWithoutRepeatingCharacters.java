6668c638e72d714ee1ab84ca6bef4f103ba0836d
palraj <palrajannadurai@gmail.com>
2026-02-22 11:49:45 +0530

    sliding window and string

--- src/main/java/dsa/string/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/dsa/string/LongestSubstringWithoutRepeatingCharacters.java
@@ -6,17 +6,16 @@ import java.util.Map;
 public class LongestSubstringWithoutRepeatingCharacters {
     public int lengthOfLongestSubstring(String s) {
         final int n = s.length();
-        Map<Character, Integer> idx = new HashMap<>();
-        int maxLen = 0;
+        Map<Character, Integer> seen = new HashMap<>();
         int left = 0;
+        int maxLen = 0;
         for (int right = 0; right < n; right++) {
-            char c = s.charAt(right);
-            if (idx.containsKey(c)) {
-                // To make sure the left idx always moves forward we need to take max always
-                left = Math.max(left, idx.get(c) + 1);
+            char curr = s.charAt(right);
+            if (seen.containsKey(curr)) {
+                left = Math.max(left, seen.get(curr) + 1);
             }
-            idx.put(c, right);
             maxLen = Math.max(maxLen, right - left + 1);
+            seen.put(curr, right);
         }
         return maxLen;
     }
