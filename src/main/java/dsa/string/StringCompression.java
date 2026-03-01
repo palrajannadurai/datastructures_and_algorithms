07cf882502fa476821919b9dfc3828d2ffe85acf
palraj <palrajannadurai@gmail.com>
2026-03-01 08:56:48 +0530

    string compression

---
+++ src/main/java/dsa/string/StringCompression.java
@@ -0,0 +1,32 @@
+package dsa.string;
+
+import java.util.Arrays;
+
+public class StringCompression {
+    public int compress(char[] chars) {
+        int read = 0;
+        int write = 0;
+        final int n = chars.length;
+        while (read < n) {
+            char current = chars[read];
+            int count = 0;
+            while (read < n && current == chars[read]) {
+                count++;
+                read++;
+            }
+            chars[write++] = current;
+            if (count > 1) {
+                for (char c : String.valueOf(count).toCharArray()) {
+                    chars[write++] = c;
+                }
+            }
+        }
+        return write;
+    }
+
+    public static void main(String[] args) {
+        char[] chars = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+        System.out.println(new StringCompression().compress(chars));
+        System.out.println(Arrays.toString(chars));
+    }
+}
