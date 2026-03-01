ed2343d54cfe5d0fec0b45ddb4a410f3f298166c
palraj <palrajannadurai@gmail.com>
2026-03-01 08:59:10 +0530

    longest almost palindromic substring

---
+++ src/main/java/dsa/string/LongestAlmostPalindromicSubstring.java
@@ -0,0 +1,55 @@
+package dsa.string;
+
+public class LongestAlmostPalindromicSubstring {
+
+    Boolean[][] memo;
+
+    public int almostPalindromic(String s) {
+        int maxLen = 0;
+        final int n = s.length();
+        for (int start = 0; start < n; start++) {
+            for (int end = start; end < n; end++) {
+                if (end - start + 1 > 2 && isAlmostPalindrome(s, start, end)) {
+                    maxLen = Math.max(maxLen, end - start + 1);
+                }
+            }
+        }
+        return maxLen;
+    }
+
+    private boolean isAlmostPalindrome(String s, int start, int end) {
+        if (memo[start][end] != null) {
+            return memo[start][end];
+        }
+        while (start < end) {
+            if (s.charAt(start) == s.charAt(end)) {
+                start++;
+                end--;
+            } else  {
+                memo[start][end] = isPalindrome(s, start + 1, end) || isPalindrome(s, start, end - 1);
+                return memo[start][end];
+            }
+        }
+        memo[start][end] = true;
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        String s = "abca";
+        System.out.println(new LongestAlmostPalindromicSubstring().almostPalindromic(s));
+    }
+}
