8b6500ca9d1d2358988bbdb30c45ca71aec88ab6
palraj <palrajannadurai@gmail.com>
2026-03-13 18:57:00 +0530

    binary search on the answer

--- src/main/java/dsa/search/searchspace/KthSmallestElementInSortedMatrix.java
+++ src/main/java/dsa/search/searchspace/KthSmallestElementInSortedMatrix.java
@@ -1,24 +1,42 @@
 package dsa.search.searchspace;
 
-import java.util.PriorityQueue;
-import java.util.Queue;
-
 public class KthSmallestElementInSortedMatrix {
     public int kthSmallest(int[][] matrix, int k) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> b - a);
-        for (int[] nums: matrix) {
-            for (int num: nums) {
-                queue.offer(num);
-                while (queue.size() > k) {
-                    queue.poll();
-                }
+        int n = matrix.length;
+        int low = matrix[0][0], high = matrix[n - 1][n - 1];
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (countLessEqual(matrix, k, mid)) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
-        return queue.peek();
+        return low;
     }
 
+    /* This will if the row and col both are sorted */
+    private boolean countLessEqual(int[][] matrix, int k, int target) {
+        final int n = matrix.length;
+        int row = n - 1, col = 0, count = 0;
+        while (row >= 0 && col < n) {
+            if (matrix[row][col] <= target) {
+                count += row + 1;
+                col++;
+            } else {
+                row--;
+            }
+        }
+        return count < k;
+    }
+
+
     public static void main(String[] args) {
-        int[][] matrix = {{1, 5, 9}, {10, 11, 13}, {12, 13, 15}};
+        int[][] matrix = {
+                {1, 5, 9},
+                {10, 11, 13},
+                {12, 13, 15}
+        };
         int k = 8;
         System.out.println(new KthSmallestElementInSortedMatrix().kthSmallest(matrix, k));
     }
